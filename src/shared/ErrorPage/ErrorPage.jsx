db776ac0cb63cd41e22aa191a0d0e53d205e324d
RIEAD <mdriead.bd@gmail.com>
2024-06-10 02:02:07 +0600

    Error Page data edited

--- src/shared/ErrorPage/ErrorPage.jsx
+++ src/shared/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import Menubar from "../Menubar/Menubar";
+import { Button } from "@material-tailwind/react";
+
 const ErrorPage = () => {
     return (
         <div>
-            <h1 className="text-2xl font-bold text-center mt-5"> Error 404 </h1>
+            <Menubar/>
+            <div className="min-h-[calc(100vh-355px)] flex justify-center items-center flex-col">
+                <h1 className="text-7xl font-semibold text-red-500">404</h1>
+                <h1>Something went wrong.</h1>
+                <p>Please go back to homepage</p>
+                <Link to={'/'}> <Button className="bg-green-500 mt-5"> Home </Button> </Link>
+            </div>
+            <Footer/>
         </div>
     );
 };
